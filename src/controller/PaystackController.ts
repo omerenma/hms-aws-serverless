619b806fc9eda22f46db9e45c1be945264becf76
omerenma <godwin2341@gmail.com>
2023-07-14 13:27:36 +0100

    paystack integration

--- src/controller/PaystackController.ts
+++ src/controller/PaystackController.ts
@@ -34,11 +34,11 @@ class PaystackController {
 
   verifyPayment = async (req:Request, res:Response) => {
     // console.log(req.body, 'query parameter')
-    if(!req.params.reference){
+    if(!req.body.reference){
         throw new Error('Missing transaction reference')
     }
 
-    const data= await paystackApi.verifyPayment(req.params.reference as string)
+    const data= await paystackApi.verifyPayment(req.body.reference as string)
     console.log('Verify :', data)
 
     res.status(200).send({
